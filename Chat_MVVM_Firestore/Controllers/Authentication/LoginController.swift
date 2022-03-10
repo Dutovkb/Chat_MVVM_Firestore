c8278ca11cf0f802791db506d9521767fec5b858
Kirill Dutov <dutov.kb@yandex.ru>
2022-03-10 13:53:48 +0300

    Setup login UI screen, add UIView extensions

--- Chat_MVVM_Firestore/Controllers/Authentication/LoginController.swift
+++ Chat_MVVM_Firestore/Controllers/Authentication/LoginController.swift
@@ -12,6 +12,17 @@ final class LoginController: UIViewController {
     private enum Constants {
         static let iconHeight: CGFloat = 120
         static let iconWidth: CGFloat = 120
+        static let iconPaddingTop: CGFloat = 32
+        
+        static let buttonCornerRadius: CGFloat = 5
+        static let buttonTitleFontSize: CGFloat = 16
+        
+        static let stackViewSpacing: CGFloat = 16
+        static let stackViewPadding: CGFloat = 32
+        
+        static let emailContainerViewHeight: CGFloat = 50
+        static let passwordContainerViewHeight: CGFloat = 50
+        static let loginButtonHeight: CGFloat = 50
     }
     
     private let iconImage: UIImageView = {
@@ -19,6 +30,30 @@ final class LoginController: UIViewController {
         icon.image = UIImage(systemName: "bubble.right")
         icon.tintColor = .white
         return icon
+    }()
+    
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        view.setHeight(height: Constants.emailContainerViewHeight)
+        view.backgroundColor = .cyan
+        return view
+    }()
+    
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.setHeight(height: Constants.passwordContainerViewHeight)
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.setHeight(height: Constants.loginButtonHeight)
+        button.layer.cornerRadius = Constants.buttonCornerRadius
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: Constants.buttonTitleFontSize)
+        button.backgroundColor = .systemRed
+        return button
     }()
     
     override func viewDidLoad() {
@@ -32,6 +67,7 @@ final class LoginController: UIViewController {
         configureNavigationController()
         configureGradientLayer()
         configureIconConstraints()
+        configureStackView()
     }
     
     private func configureNavigationController() {
@@ -40,11 +76,9 @@ final class LoginController: UIViewController {
     
     private func configureIconConstraints() {
         view.addSubview(iconImage)
-        iconImage.translatesAutoresizingMaskIntoConstraints = false
-        iconImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        iconImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        iconImage.heightAnchor.constraint(equalToConstant: Constants.iconHeight).isActive = true
-        iconImage.widthAnchor.constraint(equalToConstant: Constants.iconWidth).isActive = true
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: Constants.iconPaddingTop)
+        iconImage.setDimensions(height: Constants.iconHeight, width: Constants.iconWidth)
     }
     
     private func configureGradientLayer() {
@@ -53,5 +87,24 @@ final class LoginController: UIViewController {
         gradient.locations = [0, 1]
         view.layer.addSublayer(gradient)
         gradient.frame = view.frame
+    }
+    
+    private func configureStackView() {
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView,
+                                                       passwordContainerView,
+                                                       loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = Constants.stackViewSpacing
+        view.addSubview(stackView)
+        configureStackViewConstraints(for: stackView)
+    }
+    
+    private func configureStackViewConstraints(for stackView: UIStackView) {
+        stackView.anchor(top: iconImage.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: Constants.stackViewPadding,
+                         paddingLeft: Constants.stackViewPadding,
+                         paddingRight: -Constants.stackViewPadding)
     }
 }
