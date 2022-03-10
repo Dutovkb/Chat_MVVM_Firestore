48dfa24462fd3f7d0389596349c90fb0700d4fa6
Kirill Dutov <dutov.kb@yandex.ru>
2022-03-10 11:45:40 +0300

    Initial Commit

---
+++ Chat_MVVM_Firestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat_MVVM_Firestore
+//
+//  Created by Кирилл Дутов on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
